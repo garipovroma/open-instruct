bf4316de844d9f6ad6c3f96855503ba98ef2e370
garipovroma <devilgar@gmail.com>
2026-04-15 20:44:49 +0300

    correct isfinite check

--- open_instruct/dpo_utils.py
+++ open_instruct/dpo_utils.py
@@ -758,9 +758,7 @@ def compute_loss(
     if loss_type in (DPOLossType.dpo, DPOLossType.dpo_norm):
         assert reference_cache is not None
         ref_logps = reference_cache[batch["index"]]
-        assert ref_logps["chosen_logps"] != -torch.inf
-        assert ref_logps["rejected_logps"] != -torch.inf
-        print(ref_logps["chosen_logps"], ref_logps["rejected_logps"])
+        assert torch.isfinite(ref_logps["chosen_logps"]) and torch.isfinite(ref_logps["rejected_logps"])
         return dpo_loss(
             policy_chosen_logps,
             policy_rejected_logps,
@@ -780,9 +778,7 @@ def compute_loss(
     elif loss_type == DPOLossType.wpo:
         assert reference_cache is not None
         ref_logps = reference_cache[batch["index"]]
-        assert ref_logps["chosen_logps"] != -torch.inf
-        assert ref_logps["rejected_logps"] != -torch.inf
-        print(ref_logps["chosen_logps"], ref_logps["rejected_logps"])
+        assert torch.isfinite(ref_logps["chosen_logps"]) and torch.isfinite(ref_logps["rejected_logps"])
         return wpo_loss(
             policy_chosen_logps,
             policy_rejected_logps,
